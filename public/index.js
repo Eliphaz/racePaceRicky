9a092da5665aeca0a96f652d43f80de1ea839fd4
Eliphaz <eliahlander3@gmail.com>
2022-03-21 15:27:34 -0600

    get values from form

--- public/index.js
+++ public/index.js
@@ -1,1 +1,17 @@
 console.log('javascript loaded')
+
+raceForm = document.querySelector('form')
+raceForm.addEventListener('submit',(event)=>{
+    let raceDistance = event.target.elements['distance'].value
+    let raceUnit = event.target.elements['unit'].value
+    let raceDifficulty = event.target.elements['course_difficulty'].value
+    let raceHours = event.target.elements['hours'].value
+    let raceMinutes = event.target.elements['minutes'].value
+    let raceSeconds = event.target.elements['seconds'].value
+    event.preventDefault()
+
+})
+
+const estimateTimes = (distance, time) =>{
+    let distances = [.99, 1.98, 3.11, 6.21,13.1,26.2,31.1,50,62.14]
+}
