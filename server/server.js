0da075d964e1b06e5de0f37bf2d0a51a73148702
Eliphaz <eliahlander3@gmail.com>
2022-03-24 12:29:58 -0600

    added swquelize boilerplate

--- server/server.js
+++ server/server.js
@@ -75,6 +75,21 @@ app.put('/createAccountAuth', (req,res)=>{
 })
 
 
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(CONNECTION_STRING,{
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+  })
+
+
+
 const port = process.env.PORT || 5050
 
 app.listen(port, () => {
